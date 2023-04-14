d85bc7952276d0609a91b9c464e5220ac1aca3d1
Brianna Zamora <brianna.zamora@rewardstyle.com>
2023-04-14 14:53:48 -0700

    Feature: make it static

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Kiwi",
     platforms: [.iOS(.v11)],
-    products: [.library(name: "Kiwi", targets: ["Kiwi"])],
+    products: [.library(name: "Kiwi", type: .static, targets: ["Kiwi"])],
     targets: [.target(name: "Kiwi", path: "./Kiwi")]
 )
